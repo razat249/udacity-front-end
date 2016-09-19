1474ef1acf14dfa5f0345f3fc08a852c46945e3f
Rajat Patwa <razat249@gmail.com>
2016-09-19 23:24:27 +0530

    make responsive

--- p7-neighborhood-map/js/app.js
+++ p7-neighborhood-map/js/app.js
@@ -82,15 +82,6 @@ function toggleBounce(marker) {
   }
 }
 
-// var locations = [
-//   {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-//   {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-//   {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-//   {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-//   {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-//   {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-// ];
-
 var stringStartsWith = function (string, startsWith) {          
     string = string || "";
     if (startsWith.length > string.length)
@@ -112,7 +103,7 @@ var viewModel = function() {
         return this.locationsList();
     } else {
         return ko.utils.arrayFilter(this.locationsList(), function(item) {
-            return stringStartsWith(item.title.toLowerCase(), filter);
+            return stringStartsWith(item.address.toLowerCase(), filter);
         });
     }
   }, this);
